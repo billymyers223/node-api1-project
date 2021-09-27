c6bbbb43bb29a9c6eb3ceb602ae47c73cd1bcb3b
billmyers223 <billymyers223@gmail.com>
2021-09-27 15:49:50 -0500

    Created server about to make requests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json())
+
+server.get('/', (req, res) =>{
+    res.json({message:'hello world'})
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
